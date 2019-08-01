a0317ff2c241c365a367c3abc10d9685587fcebf
pie <pie@localhost>
2019-08-01 11:51:43 +0530

    s

--- src/main/java/com/example/bank/controller/CustomerController.java
+++ src/main/java/com/example/bank/controller/CustomerController.java
@@ -3,8 +3,10 @@ package com.example.bank.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bank.dto.CustomerDto;
+import com.example.bank.dto.TransactionDto;
 import com.example.bank.repository.CustomerRepository;
 import com.example.bank.service.CustomerService;
 import com.example.bank.service.LoginService;
@@ -21,10 +23,10 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerService;
 
+	@ResponseBody
 	@PostMapping(value = "/withdraw")
-	public String withDraw(CustomerDto customerDto) {
-		customerService.withDraw(customerDto);
-		return "successful";
+	public String withDraw(TransactionDto transactionDto) {
+		return customerService.withDraw(transactionDto);
 	}
 
 	@PostMapping(value = "/login")
--- src/main/java/com/example/bank/service/CustomerService.java
+++ src/main/java/com/example/bank/service/CustomerService.java
@@ -1,12 +1,29 @@
 package com.example.bank.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.bank.dto.CustomerDto;
+import com.example.bank.dto.TransactionDto;
+import com.example.bank.model.Customer;
+import com.example.bank.repository.CustomerRepository;
 
 @Service
 public class CustomerService {
-	public CustomerDto withDraw(CustomerDto customerDto) {
-		return customerDto;
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	public String withDraw(TransactionDto transactionDto) {
+		Optional<Customer> optionalCustomer = customerRepository.findById(transactionDto.getCustomerId());
+		if (!optionalCustomer.isPresent()) {
+			return "invalidId";
+		}
+		Customer customer = optionalCustomer.get();
+		if (customer.getCustomerBalance() >= transactionDto.getTransactionAmount()) {
+			customer.setCustomerBalance(customer.getCustomerBalance() - transactionDto.getTransactionAmount());
+			return "success";
+		}
+		return "insufficient balance";
 	}
 }

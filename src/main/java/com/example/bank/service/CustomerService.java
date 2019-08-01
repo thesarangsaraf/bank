cb1bc8a337a78ba08b05e9dccd8f1777b68c1b1a
pie <pie@localhost>
2019-08-01 11:59:41 +0530

    s

--- src/main/java/com/example/bank/service/CustomerService.java
+++ src/main/java/com/example/bank/service/CustomerService.java
@@ -22,8 +22,21 @@ public class CustomerService {
 		Customer customer = optionalCustomer.get();
 		if (customer.getCustomerBalance() >= transactionDto.getTransactionAmount()) {
 			customer.setCustomerBalance(customer.getCustomerBalance() - transactionDto.getTransactionAmount());
+			customerRepository.save(customer);
 			return "success";
 		}
 		return "insufficient balance";
+	}
+
+	public String deposit(TransactionDto transactionDto) {
+		Optional<Customer> optionalCustomer = customerRepository.findById(transactionDto.getCustomerId());
+		if (!optionalCustomer.isPresent()) {
+			return "invalidId";
+		}
+		Customer customer = optionalCustomer.get();
+		customer.setCustomerBalance(customer.getCustomerBalance() + transactionDto.getTransactionAmount());
+		customerRepository.save(customer);
+		return "success";
+
 	}
 }

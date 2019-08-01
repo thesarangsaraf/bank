34239e620dd98d1465967625d637ca3afa46874b
Sarang Saraf <Sarang Saraf@DESKTOP-IBELNG4>
2019-08-01 09:49:53 +0530

    s

--- src/main/java/com/example/bank/model/Customer.java
+++ src/main/java/com/example/bank/model/Customer.java
@@ -3,5 +3,44 @@ package com.example.bank.model;
 public class Customer {
 	Integer customerId;
 	String customerName;
-	
+	String customerPassword;
+	Integer customerBalance;
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerPassword() {
+		return customerPassword;
+	}
+
+	public void setCustomerPassword(String customerPassword) {
+		this.customerPassword = customerPassword;
+	}
+
+	public Integer getCustomerBalance() {
+		return customerBalance;
+	}
+
+	public void setCustomerBalance(Integer customerBalance) {
+		this.customerBalance = customerBalance;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", customerName=" + customerName + ", customerPassword="
+				+ customerPassword + ", customerBalance=" + customerBalance + "]";
+	}
 }
